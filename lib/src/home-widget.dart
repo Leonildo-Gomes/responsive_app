52faa1f17bcabd7d0da319dca55bd5832e7e1f3f
Leonildo Gomes <leonildo.gomes@mf.gov.cv>
2020-10-14 15:18:49 -0100

    Responsive App

---
+++ lib/src/home-widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Responsive"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
